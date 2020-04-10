bd43bdb50555efa4e4d4261c777e6e72e98f746a
Loki <surfevant@gmail.com>
2020-04-09 21:44:19 -0400

    data formatted

--- formatCSV.py
+++ formatCSV.py
@@ -1,12 +1,12 @@
 import re
 
 outArr = []
-with open("./DataSets/EdistoOffshore.csv", "r") as file:
+with open("./DataSets/Edisto.csv", "r") as file:
     for line in file:
         strOut = re.sub("\s+", ",", line.strip())
         print(str)
         outArr.append(strOut)
 
-with open('formatted_EdistoOffshore.csv', 'w') as file:
+with open('formatted_Edisto.csv', 'w') as file:
     for i in outArr:
         file.write(i+'\n')
